85040b5f84e0fd1f58e1c077c8f3f5abcf73f559
v473r10 <fac.valerio@gmail.com>
2024-04-22 14:56:21 -0300

    Clear

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'AngularSupabaseTemplate' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularSupabaseTemplate');
+    expect(app.title).toEqual('NgSupabase');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularSupabaseTemplate');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NgSupabase');
   });
 });
